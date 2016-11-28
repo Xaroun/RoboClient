69e25dd123041c6d2e675a6c4b0b34a0305b400b
Xaroun <mateusz1003@gmail.com>
2016-11-28 21:13:19 +0100

    update post mechanism

--- src/Client.java
+++ src/Client.java
@@ -15,34 +15,36 @@ public class Client {
 
     public static void main(String args[]) {
         Client client = new Client();
-        //client.sendPost();
-
-
-        Thread polling = new Thread() {
-            @Override
-            public void run() {
-                while(true) {
-                    client.startPolling();
-                    try {
-                        this.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            }
-        };
-
-        polling.start();
+        client.sendNewPost();
+//        client.sendPost();
+//
+//
+//        Thread polling = new Thread() {
+//            @Override
+//            public void run() {
+//                while(true) {
+//                    client.startPolling();
+//                    try {
+//                        this.sleep(5000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//            }
+//        };
+//
+//        polling.start();
     }
 
 
     private void startPolling() {
 
             String myProperUuid = "123e4567-e89b-12d3-a456-426655440000";
-            String myWrongUuid = "aaaa-2452";
+            String myRobotId = "1234";
+            String myGameId = "TIC_TAC";
 
-            String url = "http://127.0.0.1:5000/api/robots/" + myProperUuid + "/jobs";
+            String url = "http://127.0.0.1:5000/api/robots/" + myRobotId + "/games/" + myGameId;
 
             try {
                 URL obj = new URL(url);
@@ -87,7 +89,7 @@ public class Client {
     }
 
     private void sendPost() {
-        String url = "http://192.168.2.6:8080/robots";
+        String url = "http://127.0.0.1:5000/robots";
 
         try {
 //            System.out.println(Inet4Address.getLocalHost().getHostAddress());
@@ -171,5 +173,67 @@ public class Client {
             throw new RuntimeException(e);
         }
         return ip;
+    }
+
+
+    private void sendNewPost() {
+        String myRobotId = "1234";
+        String myGameId = "TIC_TAC_TOE";
+
+        String url = "http://127.0.0.1:5000/api/robots/" + myRobotId + "/games/" + myGameId + "/new";
+
+        try {
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            //add request header
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestProperty("Content-Type", "application/json");
+
+//            String urlParameters = "{\n" +
+//                    "    \"ip\": \"178.206.211.09\",\n" +
+//                    "    \"sn\": \"EV3POZNAN\"\n" +
+//                    "}";
+
+            String body = "{\n" +
+                                "\"ip\": \"" + "aaaaaa" + "\",\n" +
+                                "\"sn\": \"EV3POZ\"\n" +
+                           "}";
+
+//            System.out.println(urlParameters);
+
+
+            //send post request
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(body);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Post parameters : " + body);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            System.out.println(response.toString());
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
